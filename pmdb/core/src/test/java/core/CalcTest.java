f7c2490a6339fdaef6c1d7c394f9f302768771b6
oddis14 <gynnild14@gmail.com>
2021-09-16 20:23:15 +0200

    edited readme

---
+++ pmdb/core/src/test/java/core/CalcTest.java
@@ -0,0 +1,1 @@
+//TODO Skrive tester
---
+++ pmdb/ui/src/main/java/ui/AppController.java
@@ -0,0 +1,12 @@
+package ui;
+
+import core.Calc;
+
+public class AppController {
+
+    private Calc calc;
+
+    public AppController() {
+        calc = new Calc();
+    }
+}
