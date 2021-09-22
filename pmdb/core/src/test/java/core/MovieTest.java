3150905735f5d6ff3c3126dbf661a7901f168f83
Torstein Korten <torstko@stud.ntnu.no>
2021-09-22 11:00:15 +0200

    Removed unused imports in MovieTest

--- pmdb/core/src/test/java/core/MovieTest.java
+++ pmdb/core/src/test/java/core/MovieTest.java
@@ -1,16 +1,11 @@
 package core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
