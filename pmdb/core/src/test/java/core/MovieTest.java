2ac827091b5b7fa9f007c2bf1aa5cd4275f5a6e1
Torstein Korten <torstko@stud.ntnu.no>
2021-09-22 10:17:18 +0200

    Written tests for setTitle, setDescription, setDuration, addLabel, removeLabel

--- pmdb/core/src/test/java/core/MovieTest.java
+++ pmdb/core/src/test/java/core/MovieTest.java
@@ -17,31 +17,95 @@ import org.junit.jupiter.api.Test;
 
 public class MovieTest {
 
+    private Movie movie;
+    String initTitle = "initTitle";
+    String initDescription = "initDescription";
+    LocalTime initDuration = LocalTime.of(02, 00);
+    Label initLabel = new Label("initLabel");
+    Collection<Label> initLabels = new ArrayList<Label>(Arrays.asList(initLabel));
+
     @Test
     public void testConstructor() {
         String title = "Film1";
         String description = "Dette er film nummer 1.";
         LocalTime duration = LocalTime.of(2, 10);
-        //TODO: implement testing with labels
-        Collection<Label> labels = new ArrayList<Label>(Arrays.asList());
-
+        Collection<Label> labels = new ArrayList<Label>(Arrays.asList(new Label("label")));
         Movie movie = new Movie(title, description, duration, labels);
 
         assertEquals(title, movie.getTitle());
         assertEquals(description, movie.getDescription());
         assertEquals(duration, movie.getDuration());
-
-        String illegalMovieTitle = "movie:movie";
-
-        assertThrows(IllegalArgumentException.class, () -> new Movie(illegalMovieTitle, description, duration, labels));
+        assertEquals(labels, movie.getLabels());
     }
 
     @BeforeEach
     public void setUp() {
-        String title = "initTitle";
-        String description = "initDescription";
-        LocalTime duration = LocalTime.of(02, 00);
-        Collection<Label> labels = new ArrayList<Label>(Arrays.asList(new Label("initLabel")));
-        Movie movie = new Movie(title, description, duration, labels);
+        movie = new Movie(initTitle, initDescription, initDuration, initLabels);
+    }
+
+    @Test
+    public void testSetTitle() {
+        String legalTitle = "Title1.!?";
+        String illegalTitle1 = "title:";
+        String illegalTitle2 = "title,";
+
+        assertEquals(initTitle, movie.getTitle());
+        movie.setTitle(legalTitle);
+        assertEquals(legalTitle, movie.getTitle());
+        assertThrows(IllegalArgumentException.class, () -> movie.setTitle(illegalTitle1));
+        assertThrows(IllegalArgumentException.class, () -> movie.setTitle(illegalTitle2));
+    }
+
+    @Test
+    public void testSetDescription() {
+        String newDescription = "newDescription";
+
+        assertEquals(initDescription, movie.getDescription());
+        movie.setDescription(newDescription);
+        assertEquals(newDescription, movie.getDescription());
+    }
+
+    @Test
+    public void testSetDuration() {
+        LocalTime newDuration = LocalTime.of(3, 30);
+
+        assertEquals(initDuration, movie.getDuration());
+        movie.setDuration(newDuration);
+        assertEquals(newDuration, movie.getDuration());
+    }
+
+    @Test
+    public void testAddLabel() {
+        Label labelOne = new Label("labelOne");
+        Label labelTwo = new Label("labelTwo");
+        Collection<Label> newLabels = new ArrayList<Label>(initLabels);
+        newLabels.add(labelOne);
+        newLabels.add(labelTwo);
+
+        assertEquals(initLabels, movie.getLabels());
+        movie.addLabel(labelOne);
+        movie.addLabel(labelTwo);
+        assertEquals(newLabels, movie.getLabels());
+    }
+
+    @Test
+    public void testRemoveLabel() {
+        Label labelOne = new Label("labelOne");
+        Label labelTwo = new Label("labelTwo");
+        Collection<Label> newLabels = new ArrayList<Label>(initLabels);
+        newLabels.add(labelOne);
+        newLabels.add(labelTwo);
+        movie.addLabel(labelOne);
+        movie.addLabel(labelTwo);
+
+        assertEquals(newLabels, movie.getLabels());
+        movie.removeLabel(labelTwo);
+        newLabels.remove(labelTwo);
+        assertEquals(newLabels, movie.getLabels());
+        movie.removeLabel(initLabel);
+        newLabels.remove(initLabel);
+        assertEquals(newLabels, movie.getLabels());
+        movie.removeLabel(new Label("notAddedLabel"));
+        assertEquals(newLabels, movie.getLabels());
     }
 }
