8f98ce80a88e4d306c02a4af5a737e78aff9f409
oddis14 <gynnild14@gmail.com>
2021-09-23 12:53:47 +0200

    added storage.java

--- pmdb/core/src/main/java/json/Storage.java
+++ pmdb/core/src/main/java/json/Storage.java
@@ -1,15 +1,30 @@
 package json;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 import core.Movie;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SequenceWriter;
 public class Storage {
+    private Collection<Movie> movies;
+    public Storage(){
+        movies = new ArrayList<>();
+    }
     public void save(Movie movie){
-        ObjectMapper mapper = new ObjectMapper();
+        //TODO This saves a new array every time it is called.
         try {
-            mapper.writeValue(Paths.get("movies.json").toFile(), movie);
+            File file = new File("movies.json");
+            ObjectMapper mapper = new ObjectMapper();
+            FileWriter fileWriter = new FileWriter(file, true);
+            SequenceWriter seqWriter = mapper.writer().writeValuesAsArray(fileWriter);
+            seqWriter.write(movie);
+            seqWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
