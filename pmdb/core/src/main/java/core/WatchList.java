cebdff0bbdb72c2be16dbafc0da1f317009cacbd
Torstein Korten <torstko@stud.ntnu.no>
2021-09-23 15:44:04 +0200

    Added getMovie(String title) to WatchList

--- pmdb/core/src/main/java/core/WatchList.java
+++ pmdb/core/src/main/java/core/WatchList.java
@@ -3,6 +3,10 @@ package core;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import java.time.LocalTime;
+import java.util.Arrays;
+
+
 public class WatchList {
     private Collection<Movie> movies;
 
@@ -14,5 +18,25 @@ public class WatchList {
     }
     public Collection<Movie> getMovies(){
         return new ArrayList<>(movies);
+    }
+    public Movie getMovie(String title){
+        // for (Movie movie : movies) {
+        //     if(movie.getTitle().equals(title)){
+        //         return movie;
+        //     }
+        // }
+        return movies.stream().filter(m -> m.getTitle().equals(title)).findAny().get();
+    }
+
+    public static void main(String[] args) {
+        String title1 = "title1";
+        String title2 = "title2";
+        String description = "initDescription";
+        LocalTime duration = LocalTime.of(02, 00);
+        Label label = new Label("initLabel");
+        Collection<Label> labels = new ArrayList<Label>(Arrays.asList(label));
+
+        Movie movie1 = new Movie(title1, description, duration, labels);
+        Movie movie2 = new Movie(title2, description, duration, labels);
     }
 }
