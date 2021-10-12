0e682bdad1535063b1dd8ca259463a3ad5082122
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2021-10-12 18:00:32 +0200

    deleted unused imports

--- pmdb/core/src/main/java/core/MovieList.java
+++ pmdb/core/src/main/java/core/MovieList.java
@@ -1,10 +1,8 @@
 package core;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
