3fce83f9978eb4adf13bf00dc7cce3f6ec7d33b0
Torstein Korten <torstko@stud.ntnu.no>
2021-10-13 23:32:02 +0200

    Deleted unused fxml-files, added testing for adding movie

--- pmdb/ui/src/test/java/ui/AppTest.java
+++ pmdb/ui/src/test/java/ui/AppTest.java
@@ -6,12 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.File;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import core.IMovie;
 
 public class AppTest extends ApplicationTest {
 
@@ -45,5 +48,12 @@ public class AppTest extends ApplicationTest {
     public void testAppController_initial() {
         assertNotNull(movieListController);
         assertNotNull(reviewListController);
+    }
+
+    @Test
+    public void testMovieList_initial(){
+        assertEquals(1, movieListController.getMovies().size(), "Wrong number of loaded movies");
+        IMovie movie = movieListController.getMovies().stream().findFirst().get();
+        assertEquals("test movie", movie.getTitle(), "Wrong title loaded.");
     }
 }
--- pmdb/ui/src/test/java/ui/MovieListTest.java
+++ pmdb/ui/src/test/java/ui/MovieListTest.java
@@ -8,17 +8,25 @@ import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Predicate;
+
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import json.moviepersistance.MovieStorage;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,14 +35,20 @@ import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.util.WaitForAsyncUtils;
 
 import core.IMovie;
+import core.MovieList;
 
 public class MovieListTest extends ApplicationTest{
     
     private MovieListController movieListController;
 
+    private EditMovieController editMovieController;
+
     private final File testFile = new File("src\\test\\resources\\ui", "MovieList_test.json");
 
-    // private ReviewListController reviewListController;
+    String title = "title";
+    String description = "description";
+    LocalTime duration = LocalTime.of(2, 30);
+    boolean watched = false;
 
     @Override
     public void start(final Stage stage) throws Exception{
@@ -42,15 +56,21 @@ public class MovieListTest extends ApplicationTest{
         final Parent root = loader.load();
         AppController appController = loader.getController();
         movieListController = appController.getMovieListController();
-        // reviewListController = appController.getReviewListController();
+        editMovieController = movieListController.editMovieController;
         stage.setScene(new Scene(root));
         stage.show();
+    }
 
-        // final FXMLLoader movieListLoader = new FXMLLoader(getClass().getResource("MovieList_test.fxml"));
-        // movieListController = movieListLoader.getController();
-        
-        // final FXMLLoader reviewListLoader = new FXMLLoader(getClass().getResource("ReviewList_test.fxml"));
-        // reviewListController = reviewListLoader.getController();
+    private void enterMovieValues(String title, String description, String hours, String minutes, boolean watched){
+        clickOn("#titleField").write(title);
+        clickOn("#descriptionField").write(description);
+        clickOn("#hoursField").write(hours);
+        clickOn("#minutesField").write(minutes);
+        if(watched){clickOn("#watchedCheckBox");}
+    }
+
+    private int movieListSize(){
+        return movieListController.getMovies().size();
     }
 
     @BeforeEach
@@ -60,7 +80,21 @@ public class MovieListTest extends ApplicationTest{
         } catch (Exception e) {
             fail(e);
         }
+        assertEquals(1, movieListSize());
         clickOn("#movieListTab");
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException{
+        MovieStorage storage = new MovieStorage();
+        storage.setFile(testFile);
+        MovieList movieList = storage.loadMovies();
+        movieList.getMovies().stream().forEach(movie -> {
+            if(!movie.getTitle().equals("test movie")){
+                movieList.removeMovie(movie);
+            }
+        });
+        storage.saveMovies(movieList);
     }
 
     @Test
@@ -86,26 +120,55 @@ public class MovieListTest extends ApplicationTest{
     
     @Test
     public void testAddMovie_valid(){
-        String title = "title";
-        String description = "This is a movie";
-        LocalTime duration = LocalTime.of(2, 30);
-        boolean watched = true;
-
         clickOn("#openEditMovie");
-        clickOn("#titleField").write(title);
-        clickOn("#descriptionField").write(description);
-        clickOn("#hoursField").write(String.valueOf(duration.getHour()));
-        clickOn("#minutesField").write(String.valueOf(duration.getMinute()));
-        if(watched){clickOn("#watchedCheckBox");}
-        clickOn("#addTheMovie");
+        enterMovieValues(title, description, String.valueOf(duration.getHour()), String.valueOf(duration.getMinute()), watched);
+        clickOn("#submitMovie");
 
         assertEquals(2, movieListController.getMovieList().getMovies().size());
+        assertEquals(false, movieListController.editMovieWindow.isVisible());
 
-        IMovie movie = movieListController.getMovies().stream().findFirst().get();
+        IMovie movie = movieListController.getMovieList().getMovie(title);
         
         assertEquals(title, movie.getTitle());
         assertEquals(description, movie.getDescription());
         assertEquals(duration, movie.getDuration());
         assertEquals(watched, movie.isWatched());
+    }
+
+    @Test 
+    public void testAddMovie_titleInUse(){
+        String title = "test movie";
+        clickOn("#openEditMovie");
+        enterMovieValues(title, description, String.valueOf(duration.getHour()), String.valueOf(duration.getMinute()), watched);
+        clickOn("#submitMovie");
+        assertNotEquals("", editMovieController.errorField.getText());
+        assertEquals(1, movieListSize());
+        assertEquals(true, movieListController.editMovieWindow.isVisible());
+    }
+
+    @Test
+    public void testAddMovie_invalidDuration(){
+        String validHours = "2";
+        String validMinutes = "30";
+
+        String nonInteger = "1o";
+        clickOn("#openEditMovie");
+        enterMovieValues(title, description, nonInteger, validMinutes, watched);
+        clickOn("#submitMovie");
+        assertEquals(1, movieListSize());
+        assertTrue(movieListController.editMovieWindow.isVisible());
+        assertNotEquals("", editMovieController.errorField.getText());
+
+        String hoursOutOfRange = "30";
+        clickOn("#openEditMovie");
+        enterMovieValues(title, description, hoursOutOfRange, validMinutes, watched);
+        clickOn("#submitMovie");
+        assertEquals(1, movieListSize());
+        
+        String minutesOutOfRange = "-30";
+        clickOn("#openEditMovie");
+        enterMovieValues(title, description, validHours, minutesOutOfRange, watched);
+        clickOn("#submitMovie");
+        assertEquals(1, movieListSize());
     }
 }
