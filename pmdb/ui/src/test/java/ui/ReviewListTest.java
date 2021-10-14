0ade2d4e7f857702a5320eada4bdc54d2b9209e6
Torstein Korten <torstko@stud.ntnu.no>
2021-10-14 16:05:31 +0200

    Added tests for ReviewList

--- pmdb/ui/src/test/java/ui/ReviewListTest.java
+++ pmdb/ui/src/test/java/ui/ReviewListTest.java
@@ -2,13 +2,13 @@ package ui;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.robot.Robot;
 import javafx.stage.Stage;
@@ -17,7 +17,6 @@ import json.moviepersistance.MovieStorage;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import core.IMovie;
+import core.IReview;
 import core.MovieList;
 
 public class ReviewListTest extends ApplicationTest{
@@ -39,6 +39,9 @@ public class ReviewListTest extends ApplicationTest{
 
     private final File testFile = new File("src\\test\\resources\\ui", "MovieList_test.json");
 
+    private String comment = "comment";
+    private LocalDate whenWatched = LocalDate.of(2021, 10, 10);
+    
     @Override
     public void start(final Stage stage) throws Exception{
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
@@ -50,9 +53,11 @@ public class ReviewListTest extends ApplicationTest{
         stage.setScene(new Scene(root));
         stage.show();
     }
-
-    private void deleteInput(TextField text){
-        while(text.getText().length() != 0){
+    
+    private void deleteInput(TextArea text){
+        System.out.println(editReviewController.commentField.getText());
+        String comment = text.getText();
+        for(int i = 0; i < comment.length(); i++){
             Platform.runLater(() -> {
                 (new Robot()).keyPress(KeyCode.BACK_SPACE);
             });
@@ -101,5 +106,60 @@ public class ReviewListTest extends ApplicationTest{
         assertNotNull(movieListController);
     }
 
+    @Test
+    public void addReview_valid(){
+        clickOn("#openEditReview");
+        clickOn("#commentField").write(comment);
+        clickOn("#dateField");
+        editReviewController.dateField.setValue(whenWatched);
+        clickOn("#submitReview");
+        assertEquals(1, reviewListSize());
+        assertFalse(reviewListController.editReviewWindow.isVisible());
+    }
+
+    @Test
+    public void addReview_invalidDate(){
+        LocalDate invalidDate = LocalDate.now().plusDays(30);
+        clickOn("#openEditReview");
+        clickOn("#dateField");
+        editReviewController.dateField.setValue(invalidDate);
+        clickOn("#submitReview");
+        assertEquals(0, reviewListSize());
+        assertTrue(reviewListController.editReviewWindow.isVisible());
+    }
     
+    @Test
+    public void editReview(){
+        String newComment = "new comment";
+
+        clickOn("#openEditReview");
+        clickOn("#commentField").write(comment);
+        clickOn("#dateField");
+        editReviewController.dateField.setValue(whenWatched);
+        clickOn("#submitReview");
+        assertEquals(1, reviewListSize());
+        
+        clickOn(reviewListController.reviewDisplay.lookup("#0").lookup("#editReview"));
+        clickOn("#commentField");
+        deleteInput(editReviewController.commentField);
+        clickOn("#commentField").write(newComment);
+        clickOn("#submitReview");
+        
+        assertEquals(1, reviewListSize());
+        IReview review = movieListController.getMovieList().getMovie("test movie").getReviews().stream().findFirst().get();
+        assertEquals(newComment, review.getComment());
+    }
+    
+    @Test
+    public void deleteReview(){
+        clickOn("#openEditReview");
+        clickOn("#commentField").write(comment);
+        clickOn("#dateField");
+        editReviewController.dateField.setValue(whenWatched);
+        clickOn("#submitReview");
+        assertEquals(1, reviewListSize());
+        
+        clickOn(reviewListController.reviewDisplay.lookup("#0").lookup("#deleteReview"));
+        assertEquals(0, reviewListSize());
+    }
 }
