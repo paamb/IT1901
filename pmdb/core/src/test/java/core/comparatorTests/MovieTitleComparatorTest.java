6b0b87ef4d4997bf651857d44a9cb7ac88ee3d0e
oddis14 <omgynnil@stud.ntnu.no>
2021-10-15 12:45:55 +0200

    cleaned up MovieTitleComparatorTest.java according to checkstyles

--- pmdb/core/src/test/java/core/comparatorTests/MovieTitleComparatorTest.java
+++ pmdb/core/src/test/java/core/comparatorTests/MovieTitleComparatorTest.java
@@ -4,17 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import core.IMovie;
+import core.Movie;
+import core.MovieList;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
-
 import org.junit.jupiter.api.Test;
-
-import core.IMovie;
-import core.Movie;
-import core.MovieList;
 
 public class MovieTitleComparatorTest {
 
