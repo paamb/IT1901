9b5c1f584e39794beea03ffc21b5f37068f4a347
oddis14 <omgynnil@stud.ntnu.no>
2021-10-15 12:16:40 +0200

    cleaned up Review.java according to checkstyles

--- pmdb/core/src/main/java/core/Review.java
+++ pmdb/core/src/main/java/core/Review.java
@@ -8,12 +8,23 @@ public class Review implements IReview {
   private int rating;
   private LocalDate whenWatched;
 
+  /**
+   * Review constructor.
+   *
+   * 
+   */
   public Review(String comment, int rating, LocalDate whenWatched) {
     setComment(comment);
     setRating(rating);
     setWhenWatched(whenWatched);
   }
 
+  /**
+   * Sets comment for the review.
+   * 
+   * @param comment The cpomment to attach to the review
+   * 
+   */
   public void setComment(String comment) {
     if (!IReview.isValidComment(comment)) {
       throw new IllegalArgumentException("Comment cannot be null");
@@ -25,6 +36,12 @@ public class Review implements IReview {
     return comment;
   }
 
+  /**
+   * Sets rating for movie.
+   * 
+   * @param rating The rating for the movie
+   * 
+   */
   public void setRating(int rating) {
     if (!IReview.isValidRating(rating)) {
       throw new IllegalArgumentException("Rating must be at least 1 and at most 10.");
@@ -36,6 +53,12 @@ public class Review implements IReview {
     return rating;
   }
 
+  /**
+   * Sets when the movie was watched.
+   * 
+   * @param whenWatched The date the movie was watched
+   * 
+   */
   public void setWhenWatched(LocalDate whenWatched) {
     if (!IReview.isValidWhenWatched(whenWatched)) {
       throw new IllegalArgumentException("When Watched cannot be null or in the future.");
