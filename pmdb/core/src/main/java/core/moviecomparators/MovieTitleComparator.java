83764f38eabb85c8d23a085bd962d05afa88109a
oddis14 <omgynnil@stud.ntnu.no>
2021-10-15 12:20:00 +0200

    cleaned up MovieTitleComparator.java according to checkstyles

--- pmdb/core/src/main/java/core/moviecomparators/MovieTitleComparator.java
+++ pmdb/core/src/main/java/core/moviecomparators/MovieTitleComparator.java
@@ -1,13 +1,13 @@
 package core.moviecomparators;
 
+import core.IMovie;
 import java.util.Comparator;
 
-import core.IMovie;
 
-public class MovieTitleComparator implements Comparator<IMovie>{
+public class MovieTitleComparator implements Comparator<IMovie> {
 
-    @Override
-    public int compare(IMovie o1, IMovie o2) {
-        return o1.getTitle().toLowerCase().compareTo(o2.getTitle().toLowerCase());
-    }
+  @Override
+  public int compare(IMovie o1, IMovie o2) {
+    return o1.getTitle().toLowerCase().compareTo(o2.getTitle().toLowerCase());
+  }
 }
