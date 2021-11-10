cd644d5d5ea093165fce95cb44d80e4a2f4be8f0
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2021-11-10 14:15:47 +0100

    Javadoc for ILabel

--- pmdb/core/src/main/java/core/ILabel.java
+++ pmdb/core/src/main/java/core/ILabel.java
@@ -2,12 +2,44 @@ package core;
 
 public interface ILabel {
 
+  /**
+   * Checks if label is in movie. Checks by title.
+   * 
+   * @param movie movie to be checked
+   * @param title title to
+   * @return
+   */
+  public static boolean isLabelInMovie(IMovie movie, String title) {
+    return movie.getLabels().stream().anyMatch(label -> label.getTitle().equals(title));
+  }
+
+  /**
+   * Sets this label title.
+   * 
+   * @param title Movie title
+   * @throws IllegalArgumentException if illegal title
+   */
   public void setTitle(String title);
 
+  /**
+   * Get the title of this label.
+   * 
+   * @return this label title
+   */
   public String getTitle();
 
+  /**
+   * Sets the color of this label. Can be a random value or a color set by user.
+   * 
+   * @param color the color for this label
+   */
   public void setColor(String color);
 
+  /**
+   * Get the color of this label.
+   * 
+   * @return this label color
+   */
   public String getColor();
 
 }
