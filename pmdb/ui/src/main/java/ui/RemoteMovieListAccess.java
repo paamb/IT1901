8fe7b7b72d3d61966da20f9e9c6445cd76c78933
Torstein Korten <torstko@stud.ntnu.no>
2021-11-14 02:27:37 +0100

    Made getMovieList in RemoteMovieLIstAccess

--- pmdb/ui/src/main/java/ui/RemoteMovieListAccess.java
+++ pmdb/ui/src/main/java/ui/RemoteMovieListAccess.java
@@ -1,6 +1,8 @@
 package ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import core.MovieList;
+import json.moviepersistance.MovieStorage;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -14,4 +16,26 @@ import java.util.Collection;
 
 public class RemoteMovieListAccess {
   
+  private URI baseUri;
+
+  private ObjectMapper objectMapper;
+
+  public RemoteMovieListAccess(URI baseUri) {
+    this.baseUri = baseUri; 
+    objectMapper = MovieStorage.createObjectMapper();
+  }
+
+  public MovieList getMovieList() {
+    HttpRequest request = HttpRequest.newBuilder(baseUri)
+        .header("Accept", "application/json")
+        .GET()
+        .build();
+    try {
+      HttpResponse<String> response = HttpClient.newBuilder().build()
+          .send(request, HttpResponse.BodyHandlers.ofString());
+      return objectMapper.readValue(response.body(), MovieList.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
