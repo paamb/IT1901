6cc3b3f6a6aa36f9935f9b6089db3ed706981980
mariohar <mariohar@stud.ntnu.no>
2021-11-18 12:01:53 +0100

    endret til pmdb.port

--- pmdb/integrationtests/src/test/java/ui/AppIT.java
+++ pmdb/integrationtests/src/test/java/ui/AppIT.java
@@ -33,8 +33,8 @@ public class AppIT extends ApplicationTest {
   @BeforeEach
   public void setupMovieList() {
     try {
-      String port = System.getProperty("todo.port");
-      assertNotNull(port, "The port moive.port is null.");
+      String port = System.getProperty("pmdb.port");
+      assertNotNull(port, "The port pmdb.port is null.");
       String baseUri = "http://localhost:" + port + "/movielist";
       this.movieListController.syncWithServer(baseUri);
       assertTrue(this.movieListController.serverIsRunning());
